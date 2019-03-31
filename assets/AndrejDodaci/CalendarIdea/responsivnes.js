b84a62f43ed7882abe3894111cd908036b6e1be0
Re3eY <49090518+Re3eY@users.noreply.github.com>
2019-03-31 03:27:32 +0200

    backup

--- assets/AndrejDodaci/CalendarIdea/responsivnes.js
+++ assets/AndrejDodaci/CalendarIdea/responsivnes.js
@@ -1,23 +1,86 @@
-$(document).ready(function ()
-{
-
 function daysInMonth (month, year, day) {
         return new Date(year, month, day).getDate();
     }
+
+
     var date = new Date();
-    var daysinMon = daysInMonth(date.getMonth() + 1,date.getFullYear(), 0);
+    var daysinMon = daysInMonth(date.getMonth()+1,date.getFullYear(), 0);
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var month = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 
 
+    //Month text and Year text
+    function YearandMonth(month1,year1) {
+    //var month1 = new Date(date.getMonth()+1+" "+date.getDay()+" "+date.getFullYear());
+    var month2 = new Date(year1,month1+1,date.getDay());
+    var mm = month2.getMonth();
+    var monthName = month[mm];
+    document.getElementById("Mname").innerHTML = monthName;
+    var yy = month2.getFullYear();
+    document.getElementById("Yname").innerHTML = yy;
+    return month2;
+    }
+    YearandMonth(date.getMonth(), date.getFullYear());
+
+    var mooo = YearandMonth(date.getMonth(), date.getFullYear());
+    var mm = mooo.getMonth();
+
+    //month -1
+    function leftM()
+{
+    mm--;
+
+    if(mm < 0)
+    {
+        mm = 11;
+    }
+    else if (mm > 11)
+    {
+        mm = 0;
+    }
+    var mm2 = new Date(date.getFullYear(),mm,+date.getDay());
+    var mnthchange = mm2.getMonth();
+    var monthName = month[mnthchange];
+    document.getElementById("Mname").innerHTML = monthName;
+}
+    //month +1
+    function rightM()
+    {
+        mm++;
+        if(mm < 0)
+        {
+            mm = 11;
+        }
+        else if (mm > 11)
+        {
+            mm = 0;
+        }
+        var mm2 = new Date(date.getFullYear(),mm,+date.getDay());
+        var mnthchange = mm2.getMonth();
+        var monthName = month[mnthchange];
+        document.getElementById("Mname").innerHTML = monthName;
+
+        console.log(YearandMonth().getFullYear()+" " +mm);
+
+        genDays(YearandMonth(mnthchange,date.getFullYear()).getFullYear(),mm);
+    }
+    //Generate days
+function genDays(year,month) {
+
+        for (var i = 0; i < del.length;i++)
+        {
+            document.getElementById("day"+i).remove();
+        }
+        delete(del);
+    }
     for(x=1; x<=daysinMon;x++) {
 
         var cal = document.createElement('div');
         cal.id = "day"+x;
         cal.className = "containter";
 
-        var date1 = new Date(date.getMonth()+1+" "+x+" "+date.getFullYear());
+        var date1 = new Date(year,month,x);
         var dd = date1.getDay();
-        console.log(date1);
         var dayName = days[dd];
 
         var title = document.createElement('div');
@@ -52,6 +115,4 @@ function daysInMonth (month, year, day) {
 
 
     }
-
-
-});
+}
